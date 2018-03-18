a9dbe75f53ce586ac685a6134a5f017c2fe20832
Gaelle Letort <34513249+gletort@users.noreply.github.com>
2018-03-18 11:46:11 +0100

    update makefiles to fix compilation on MacOS

--- MaBoSS/src/maboss-config.h
+++ MaBoSS/src/maboss-config.h
@@ -1,7 +1,4 @@
-#define HAS_RAND48_T
-#define HAS_RAND48
-// @HAS_UNORDERED_MAP
-#define HAS_UNORDERED_MAP
-#include <unordered_map>
-#define STATE_MAP std::unordered_map
-#define HASH_STRUCT hash
+//#define HAS_RAND48_T
+//#define HAS_RAND48
+//#define HAS_UNORDERED_MAP
+#define STATE_MAP std::map
